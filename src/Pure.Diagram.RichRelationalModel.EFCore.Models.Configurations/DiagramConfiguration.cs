8838ab115f3514c497816041fddfaa47fc36031a
Dmitry Kurochkin <kudima03@gmail.com>
2026-02-18 12:00:48 +0300

    Comparers introduced in DiagramConfiguration

--- src/Pure.Diagram.RichRelationalModel.EFCore.Models.Configurations/DiagramConfiguration.cs
+++ src/Pure.Diagram.RichRelationalModel.EFCore.Models.Configurations/DiagramConfiguration.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Pure.Primitives.Abstractions.EFCore.Converters;
+using Pure.Primitives.Abstractions.EFCore.ValueComparers;
 
 namespace Pure.Diagram.RichRelationalModel.EFCore.Models.Configurations;
 
@@ -10,27 +11,31 @@ public sealed record DiagramConfiguration : IEntityTypeConfiguration<DiagramEFCo
     {
         _ = builder.HasKey(x => x.Id);
 
-        _ = builder
+        builder
             .Property(x => x.Id)
             .ValueGeneratedNever()
             .IsRequired()
-            .HasConversion(new GuidTypeConverter());
+            .HasConversion(new GuidTypeConverter())
+            .Metadata.SetValueComparer(new GuidValueComparer());
 
-        _ = builder
+        builder
             .Property(x => x.Title)
             .IsRequired()
             .HasConversion(new StringTypeConverter())
-            .HasMaxLength(64);
+            .HasMaxLength(64)
+            .Metadata.SetValueComparer(new StringValueComparer());
 
-        _ = builder
+        builder
             .Property(x => x.Description)
             .IsRequired()
-            .HasConversion(new StringTypeConverter());
+            .HasConversion(new StringTypeConverter())
+            .Metadata.SetValueComparer(new StringValueComparer());
 
-        _ = builder
+        builder
             .Property(x => x.TypeId)
             .IsRequired()
-            .HasConversion(new GuidTypeConverter());
+            .HasConversion(new GuidTypeConverter())
+            .Metadata.SetValueComparer(new GuidValueComparer());
 
         _ = builder.HasOne(x => x.TypeNavigation).WithMany().HasForeignKey(x => x.TypeId);
 
